964463a888eaa35ad7ca4160aa21fd1861ffd322
Caecilia68y <ydnjs12@naver.com>
2021-05-30 23:41:06 +0900

    DNA_Sequence_1.1
    딕셔너리 이외의 문자가 포함된 경우 오류 발생이 아닌 ?문자 출력

--- DNA_Sequence.py
+++ DNA_Sequence.py
@@ -1,10 +1,14 @@
-#DNA 염기서열의 순서 바꾸기
+#DNA 염기서열의 순서 바꾸기 1.1
+#딕셔너리 이외의 문자가 포함된 경우 오류 발생이 아닌 ?문자 출력
 
 def comp(DNAst) :
     dic = {'A':'T', 'T':'A', 'G':'C', 'C':'G'}
     seq_comp=""
     for i in DNAst :
-        seq_comp = seq_comp + dic[i]
+        if i in dic :
+            seq_comp = seq_comp + dic[i]
+        else :
+            seq_comp = seq_comp + '?'
     return seq_comp
 
 def rev(DNAst) :
